48ef436de995727de97cc590f3fe1caf78b40e41
Giovanni Dueck <giodueck@gmail.com>
2024-03-27 20:21:56 -0400

    Fix install.sh directory creation

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@
 # Move relevant dotfiles into home (~)
 
 cp -v .zshrc ~/.zshrc
+if [ ! -d ~/.local/share/fzf ]; then
+    mkdir -p ~/.local/share/fzf
+fi
 cp -v .local/share/fzf/key-bindings.zsh ~/.local/share/fzf/key-bindings.zsh
