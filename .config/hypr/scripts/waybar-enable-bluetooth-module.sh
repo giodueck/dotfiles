f8a502f1520a14de2c8a0906df3380f2f4376875
Giovanni Dueck <giovanni.dueck@avantys.de>
2024-08-20 07:22:33 -0400

    Hyprland: Add timeout to bluetoothctl command if the bluetooth service won't start

--- .config/hypr/scripts/waybar-enable-bluetooth-module.sh
+++ .config/hypr/scripts/waybar-enable-bluetooth-module.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-if [ -z "$(bluetoothctl list)" ]; then
+BL_LIST=$(timeout 2 bluetoothctl list)
+if [ -z "$BL_LIST" ]; then
     sed -i 's/^[^\/]*[ ]*"bluetooth",/\/\/&/' ~/.config/waybar/config.jsonc
 else
     sed -i 's/^[\/]*\([ ]*"bluetooth",\)/\1/' ~/.config/waybar/config.jsonc
