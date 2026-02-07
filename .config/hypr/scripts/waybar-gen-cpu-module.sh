fbf717de701bfc99b5e0b9364a638f6f9aaeb9c0
Giovanni Dueck <giovanni.dueck@avantys.de>
2026-02-06 23:28:48 -0300

    Waybar: generate temperature module alongside cpu

--- .config/hypr/scripts/waybar-gen-cpu-module.sh
+++ .config/hypr/scripts/waybar-gen-cpu-module.sh
@@ -21,6 +21,21 @@
     echo '            "<span color='\''#ff9977'\''>▇</span>", // orange'
     echo '            "<span color='\''#dd532e'\''>█</span>"  // red'
     echo '        ]'
+    echo '    },'
+
+    echo '    "temperature": {'
+    echo '        // "thermal-zone": 2,'
+    # Prefer Tccd temperature, but settle for Tctl if not available
+    if [ -f "/sys/class/hwmon/hwmon2/temp3_input" ]; then
+        echo '        "hwmon-path": "/sys/class/hwmon/hwmon2/temp3_input",'
+    else
+        echo '        "hwmon-path": "/sys/class/hwmon/hwmon2/temp1_input",'
+    fi
+    echo '        "warning-threshold": 80,'
+    echo '        "critical-threshold": 90,'
+    echo '        "format": "{icon} {temperatureC} ",'
+    echo '        "format-icons": ["", "", ""]'
     echo '    }'
+
     echo '}'
 } > ~/.config/waybar/cpu.jsonc
