42b59ef7e8ae182fd44d4cf4437660e27f2f2818
Giovanni Dueck <giovanni.dueck@avantys.de>
2026-05-20 17:23:09 -0300

    Hyprland: update to 0.55 lua config

--- .config/hypr/scripts/toggle-zen.sh
+++ .config/hypr/scripts/toggle-zen.sh
@@ -2,22 +2,10 @@
 
 ZEN_MODE_FILE=/tmp/zen_active
 
-# TODO: use hyprctl getoption to reset the same options later. Hardcode is easier though
-
 if [[ -f $ZEN_MODE_FILE ]]; then
     rm $ZEN_MODE_FILE
-    hyprctl keyword general:border_size 2
-    hyprctl keyword general:gaps_in 0
-    hyprctl keyword general:gaps_out 0
-    hyprctl keyword animations:enabled true
-    hyprctl keyword decoration:rounding 4
-    hyprctl keyword decoration:inactive_opacity 0.95
+    hyprctl reload
 else
     touch $ZEN_MODE_FILE
-    hyprctl keyword general:border_size 0
-    hyprctl keyword general:gaps_in 0
-    hyprctl keyword general:gaps_out 0
-    hyprctl keyword animations:enabled false
-    hyprctl keyword decoration:rounding 0
-    hyprctl keyword decoration:inactive_opacity 1
+    hyprctl eval "hl.config({general={border_size=0,gaps_in=0,gaps_out=0},animations={enabled=false},decoration={rounding=0,inactive_opacity=1}})"
 fi
